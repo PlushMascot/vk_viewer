2c5d6295cadbcd3a1caa554a081f428f24487583
PlushMascot <carpdiem1@mail.ru>
2021-08-04 23:21:51 +0400

    changes to OAuth
    Created OAuth link, which redirects to home page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, request
 from .. import db
 from ..models import User
 from . import main
@@ -8,19 +8,16 @@ from .forms import NameForm
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    return render_template('index.html')
 
+
+@main.route('/signup', methods=['GET'])
+def signup():
+    code = request.args.get('code')
+    print(code)
+    return redirect(url_for('.home'), code=302)
+
+
+@main.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
